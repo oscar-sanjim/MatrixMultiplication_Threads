95b355f2cb0dcd4467654c7f7781a0ae89427fa8
Oscar <oscar.sanjim@gmail.com>
2015-05-06 23:20:00 -0500

    Proyecto terminado - Corregido el thread aleatorio

--- matrix.h
+++ matrix.h
@@ -10,9 +10,14 @@
 #define __matrices__matrix__
 
 #include <stdio.h>
+#include <pthread.h>
+
 
 #endif /* defined(__matrices__matrix__) */
 void PrintMatrix(int *mat,int r, int c);
 void Multiplication (int *m1, int *m2, int m1r, int m1c,
-                     int m2c, int **m3, int threadNum);
+                     int m2c, int **m3, int threadNum,
+                     pthread_mutex_t *key, pthread_cond_t *done);
 void InitDiagonal(int **mat, int r, int c);
+
+
